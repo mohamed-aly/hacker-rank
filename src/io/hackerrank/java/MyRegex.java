c2020616b4cba1cd2e8042a50418b4d19a06f459
mohamed-aly <mali902016@gmail.com>
2022-03-25 06:39:14 +0200

    validate ipv4

---
+++ src/io/hackerrank/java/MyRegex.java
@@ -0,0 +1,6 @@
+package io.hackerrank.java;
+
+public class MyRegex {
+    private static final String pattern = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+    ;
+}
