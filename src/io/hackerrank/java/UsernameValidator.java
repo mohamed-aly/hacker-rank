b169db7e6225790ad9251303f0e366d165759011
mohamed-aly <mali902016@gmail.com>
2022-04-08 13:35:44 +0200

    Valid Username fix

--- src/io/hackerrank/java/UsernameValidator.java
+++ src/io/hackerrank/java/UsernameValidator.java
@@ -1,21 +1,32 @@
 package io.hackerrank.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UsernameValidator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         scanner.nextLine();
+
+        List<String> list = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            System.out.println(validateUserName(scanner.nextLine()));
+            list.add(scanner.nextLine());
+        }
+
+
+        for(String word : list){
+            System.out.println(validateUserName(word));
         }
     }
 
     private static String validateUserName(String username) {
 
-        if (username.matches("[^[a-zA-z_]+[0-9]*]{8,30}")) return "Valid";
+        if (username.matches("^[a-zA-Z][\\w]{7,29}$")) return "Valid";
 
         return "Invalid";
     }
