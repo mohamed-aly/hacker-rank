8adae9d5272fd7b011918d0c5f550b6c41d46367
mohamed-aly <mali902016@gmail.com>
2022-04-08 16:06:38 +0200

    1D array

---
+++ src/io/hackerrank/java/OneDimensionArray.java
@@ -0,0 +1,29 @@
+package io.hackerrank.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class OneDimensionArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        System.out.println(Arrays.toString(a));
+
+        bufferedReader.close();
+    }
+
+}
