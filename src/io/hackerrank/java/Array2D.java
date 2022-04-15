9f9491808608710aa5fc3b47a57e106ecaa715da
mohamed-aly <mali902016@gmail.com>
2022-04-15 14:16:35 +0200

    2D array challenge

---
+++ src/io/hackerrank/java/Array2D.java
@@ -0,0 +1,33 @@
+package io.hackerrank.java;
+
+import java.util.Arrays;
+
+public class Array2D {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {5, 6, 7, 8};
+        int[] array3 = {9, 10, 11, 12};
+        int[] array4 = {13, 14, 15, 16};
+
+        int[][] twoDArray = {array1, array2, array3, array4};
+
+        System.out.println(largestHourGlass(twoDArray));
+    }
+
+    private static int largestHourGlass(int[][] twoDArray){
+        Integer largest = null;
+        for(int i = 0; i <twoDArray.length-2; i++){
+            for(int j = 0; j < twoDArray[i].length-2; j++){
+                int sum = twoDArray[i][j] + twoDArray[i][j+1] + twoDArray[i][j+2]
+                        + twoDArray[i+1][j+1]
+                        + twoDArray[i+2][j] + twoDArray[i+2][j+1] + twoDArray[i+2][j+2];
+
+                if(largest == null || sum>largest){
+                    largest = sum;
+                }
+            }
+        }
+
+        return largest;
+    }
+}
