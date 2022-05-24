af5cff589a12784f5c834d167bc85ef7dc27a0ac
mohamed-aly <mali902016@gmail.com>
2022-05-24 14:58:52 +0200

    enhanced decompress

--- src/io/hackerrank/java/Decompress.java
+++ src/io/hackerrank/java/Decompress.java
@@ -1,20 +1,33 @@
 package io.hackerrank.java;
 
+import java.util.Arrays;
+
 public class Decompress {
     public static void main(String[] args) {
-        System.out.println(decompress("d4f5c14"));
+        System.out.println(decompress("d4edf5c14e"));
     }
 
-    private static String decompress(String compressed){
-//        String[] chars = compressed.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
-        String[] chars = compressed.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
-
+    private static String decompress(String compressed) {
+        String[] chars = compressed.split("(?<=\\D)|(?<=\\D)(?=\\d+)|(?<=\\d)(?=\\D)");
+        System.out.println(Arrays.toString(chars));
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < chars.length; i+=2){
-            for(int j = 0; j < Integer.parseInt(chars[i+1]); j++){
-                result.append(chars[i]);
-            }
-        }
+
+       int counter = 0;
+
+       while (counter < chars.length){
+           try{
+               String letter = chars[counter];
+               int reps =  Integer.parseInt(chars[counter + 1]);
+               for(int i = 0; i < reps; i++){
+                   result.append(letter);
+               }
+               counter += 2;
+           }catch (Exception ex){
+               result.append(chars[counter++]);
+           }
+       }
+
+
         return result.toString();
     }
 }
